64ba76ae7cff09cad0b1ffdf95896b4f9dfd5836
David-com85 <davisabreudelima@gmail.com>
2026-06-29 07:49:14 -0300

    Evolução do sistema de gestão industrial

--- src/funcionario.php
+++ src/funcionario.php
@@ -1,15 +1,19 @@
 <?php
 
 class Funcionario{
+
     private $id;
     private $nome;
     private $matricula;
     private $cargo;
+    private $email;
 
-    public function cadastrar($id, $nome, $matricula, $cargo){
+    public function cadastrar($id, $nome, $matricula, $cargo, $email){
         $this->id = $id;
         $this->nome = $nome;
         $this->matricula = $matricula;
         $this->cargo = $cargo;
+        $this->email = $email;
     }
+
 }
