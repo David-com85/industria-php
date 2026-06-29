8152067dc7cb8604db0290e4b9e3d7b6d91172f0
David-com85 <davisabreudelima@gmail.com>
2026-06-29 07:42:54 -0300

    atualização do cadastro de funcionário

--- src/funcionario.php
+++ src/funcionario.php
@@ -4,10 +4,12 @@ class Funcionario{
     private $id;
     private $nome;
     private $matricula;
+    private $cargo;
 
-    public function cadastrar($id, $nome, $matricula){
+    public function cadastrar($id, $nome, $matricula, $cargo){
         $this->id = $id;
         $this->nome = $nome;
         $this->matricula = $matricula;
+        $this->cargo = $cargo;
     }
 }
